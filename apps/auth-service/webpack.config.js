17ada2f5e21705f7cb0ef91e6f0e40d5ae49b11a
hasanhabib16011998 <hasan16habib@gmail.com>
2025-10-16 12:41:41 +0600

    fixed graph error

--- apps/auth-service/webpack.config.js
+++ apps/auth-service/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      "@packages": resolve(__dirname,"../../../packages"),
+      "@packages": resolve(__dirname,"../../packages"),
     },
     extensions: [".ts",".js"],
   },
