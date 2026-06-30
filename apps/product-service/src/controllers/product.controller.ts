24bf3ccb2a1579cd915b5ece85cfcdab18fdd019
hasanhabib16011998 <hasan16habib@gmail.com>
2026-06-30 17:36:38 +0600

    seller-ui: get all products table

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -268,3 +268,28 @@ export const createProduct = async (
     next(error);
   }
 }
+
+//fetch all products
+export const getShopProducts = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const products = await prisma.products.findMany({
+      where: {
+        shopId: req?.seller?.shop?.id,
+      },
+      include:{
+        images: true,
+      }
+    });
+
+    res.status(201).json({
+      success: true,
+      products,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
